ca554b119c05421a0727b8eff188dfa8be897f50
Leanne Dong <jdleanne@gmail.com>
2021-05-10 21:05:59 -0400

    replace assert with CHECK under doctest

--- HydraulicNetLib/src/inci_test.cpp
+++ HydraulicNetLib/src/inci_test.cpp
@@ -27,11 +27,19 @@
 //
 //}
 TEST_CASE("testing incidence matrix generator"){
+    std::vector<std::vector<int>> expect{{1, -1, 0, 0, 0, 0, 0},
+                {0, 1, -1, 0, 0, 0, 0},
+                {0, 1, 0, -1, 0, 0, 0},
+                {0, 0, 0, 1, 0, -1, 0},
+                {0, 0, 0, 0, 0, 1, -1},
+                {0, 0, 0, 0, -1, 0, 1},
+                {0, 0, 0, -1, 1, 0, 0}};
     size_t m{7}; size_t n{7};
     std::vector<size_t> in{0, 1, 1, 3, 5, 6, 4};
 
     std::vector<size_t> out{1, 2, 3, 5, 6, 4, 3};
-    gen_mat(m, n, in, out);
-    std::cerr << "incidence matrix has been derived correctly!\n";
+    std::vector<std::vector<int>> result = gen_mat(m, n, in, out);
+    CHECK(result == expect);
+  //  std::cerr << "incidence matrix has been derived correctly!\n";
 
 }
