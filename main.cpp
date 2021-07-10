53e89cb7a56fe25cfc99c2c7997f502443632b78
Leanne Dong <jdleanne@gmail.com>
2021-07-10 16:54:38 -0400

    detected cycles for verona, without eliminating any potential redundant rows

--- main.cpp
+++ main.cpp
@@ -7,13 +7,14 @@
 #include "../include/util.h"
 #include "../include/NetworkSolve.h"
 #include "../include/graph.h"
+#include "../include/eigenData.h"
 using Pair = std::vector<std::pair<int, int>>;
 int main()
 {
     //! get incidence matrix
  //   cerr << LoadFile().value_or("File [nw_verona.csv] could not be opened!") << "\n";
- //   ifstream in("inputs/nw_verona.csv");
-    ifstream in("inputs/nw4-3.csv");
+    ifstream in("inputs/Verona.csv");
+ //   ifstream in("inputs/nw4-3.csv");
  //   ifstream in("inputs/network1.csv");
     if(!in){
         std::cerr << "Unable to open data\n";
@@ -41,71 +42,69 @@ int main()
 //        std::cout << "\n";
 //    }
     //! define the number of Nodes and Pipes
-//    const size_t n = stoi(data[0][0]);
-//    const size_t m = stoi(data[0][1]);
-    const size_t n = 4;
-    const size_t m = 3;
+    const size_t n = stoi(data[0][0]);
+    const size_t m = stoi(data[0][1]);
+//    const size_t n = 4;
+//    const size_t m = 3;
 //    const size_t n = 4;
 //    const size_t m = data.size();
         std::cerr << "the number of nodes is " << n << "\n";
         std::cerr << "the number of pipes is " << m << "\n";
 
-    vector<string> col2, col3;
-    std::cerr << "the number of rows is " << data.size() << "\n";
-    for (size_t i = 0; i < data.size(); ++i) {
+    vector<string> col4, col5;
+    std::cerr << "the number of rows is " << m << "\n";
+    for (size_t i = 0; i < m; ++i) {
         // std::cerr << data[i][3];
-        col2.push_back(data[i][1]);
-        col3.push_back(data[i][2]);
+        col4.push_back(data[i][3]);
+        col5.push_back(data[i][4]);
         //printf("Element %d: %d", i, data[i][0]);
     }
 
     std::cerr << "now let us convert vector of string of vector of ints"
               << "\n";
-    vector<size_t> col2_int, col3_int;
-    for (auto c2 : col2) {
-        col2_int.push_back(stoi(c2));
+    vector<size_t> col4_int, col5_int;
+    for (auto c2 : col4) {
+        col4_int.push_back(stoi(c2));
     }
     //! c++ index from 0, so subtract 1 from each element of our vector
-    for (auto &e2 : col2_int) {
+    for (auto &e2 : col4_int) {
         e2 -= 1;
     }
 
-    for (auto c3 : col3) {
-        col3_int.push_back(stoi(c3));
+    for (auto c3 : col5) {
+        col5_int.push_back(stoi(c3));
     }
 
-    for (auto &e5 : col3_int) {
+    for (auto &e5 : col5_int) {
         e5 -= 1;
     }
 
     std::cerr << "column 4 is "
               << "\n";
-    //    for(auto c4: col4_int){
-    //        std::cerr << c4 << "\t";
-    //    }
-    //    std::cerr << "\n";
-    printVec(col2_int);
+    printVec(col4_int);
     std::cerr << "column 5 is "
               << "\n";
 
-    printVec(col3_int);
-    vector<vector<int>> matA = gen_mat(m, n, col2_int, col3_int);
-    printMat(matA);
+    printVec(col5_int);
+    vector<vector<int>> matA = gen_mat(m, n, col4_int, col5_int);
+ //   outputMat(matA);
     // vector<vector<int>> A = outputReduceMat(matA);
     MatrixXd A_eigen = makeEigenMatrixFromVectors(matA);
-    cerr << "The matrix A is \n"
-         << A_eigen << "\n";
+//    cerr << "The matrix A is \n"
+//         << A_eigen << "\n";
 
     MatrixXd A_tran = -A_eigen.transpose();
-    removeRow(A_tran, 0);
-    cerr << "The matrix -A^T is \n"
-         << A_tran << "\n";
+//    removeRow(A_tran, 0);
+//    cerr << "The matrix -A^T is \n"
+//         << A_tran << "\n";
+    std::ofstream filestr("outputs/inci_tran.csv");
+    saveData(filestr, A_tran);
     /// Loop detection
     /// First we need adjacency matrix
     Pair V;
-    for(size_t i{0}; i < col2_int.size(); ++i)
+    for(size_t i{0}; i < col4_int.size(); ++i)
     {
-        V.emplace_back(col2_int[i], col3_int[i]);
+        V.emplace_back(col4_int[i], col5_int[i]);
     }
     graph g(150);
     for(auto &v : V){
